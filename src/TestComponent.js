b73d4f2f0982148091dcaf71879fca7717c8ac3e
Markus <markus.kloepper@gmx.net>
2022-10-31 17:16:39 +0100

    ed

---
+++ src/TestComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TestComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="heading">Hello!</h1>
+        <p>Test this out</p>
+      </div>
+    );
+  }
+}
+
+export default TestComponent;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import ReactDOM from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<h1>Hello, everyone!</h1>,"root")
+const page = (
+  <div>
+    <img src="./react.png" alt="logo"></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(page);
