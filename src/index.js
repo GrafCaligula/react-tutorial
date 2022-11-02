ad7144d14165bb6d7fd6ea0c981f311bb4ea2b9a
Markus <markus.kloepper@gmx.net>
2022-11-02 10:58:09 +0100

    styling

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Page() {
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+
+function App() {
   return (
-    <React.StrictMode>
       <div>
-        <header>
-          <nav>
-            <img src="./react.png" alt="logo" width="40px" />
-          </nav>
-        </header>
-        <h1>Reasons I'm excited to learn React</h1>
-        <ol>
-          <li>rtherth</li>
-          <li>mu,u</li>
-          <li>wqegqg</li>
-          <li>zu,mzuk,</li>
-        </ol>
-        <footer><small>© 2022 Klöder developement, All rights reserved.</small></footer>
+        <Header />
+        <MainContent />
+        <Footer />
       </div>
-    </React.StrictMode>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Page />);
+root.render(<App />);
