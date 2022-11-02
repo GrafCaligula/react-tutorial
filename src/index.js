a2beebec933320156db54e97bf3f359969cee112
Markus <markus.kloepper@gmx.net>
2022-11-02 09:42:34 +0100

    moving on

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const page = (
-  <div>
-    <img src="./react.png" alt="logo"></img>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-  </div>
-)
+function Page() {
+  return (
+    <React.StrictMode>
+      <div>
+        <header>
+          <nav>
+            <img src="./react.png" alt="logo" width="40px" />
+          </nav>
+        </header>
+        <h1>Reasons I'm excited to learn React</h1>
+        <ol>
+          <li>rtherth</li>
+          <li>mu,u</li>
+          <li>wqegqg</li>
+          <li>zu,mzuk,</li>
+        </ol>
+        <footer><small>© 2022 Klöder developement, All rights reserved.</small></footer>
+      </div>
+    </React.StrictMode>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(page);
+root.render(<Page />);
